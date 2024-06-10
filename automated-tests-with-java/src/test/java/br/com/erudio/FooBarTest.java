624bef31449c6237df67af5537fd5ecae2699608
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2024-06-10 13:42:57 -0300

    first test

---
+++ automated-tests-with-java/src/test/java/br/com/erudio/FooBarTest.java
@@ -0,0 +1,13 @@
+package br.com.erudio;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FooBarTest {
+    @Test
+    void test() {
+//        fail("Not yet implemented");
+        System.out.println("Hello world!");
+    }
+}
