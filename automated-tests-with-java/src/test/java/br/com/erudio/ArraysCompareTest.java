7795954fa0ffc56d1696df55c15887817464a66f
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2024-06-13 22:11:50 -0300

    testing performance with @Timeout

--- automated-tests-with-java/src/test/java/br/com/erudio/ArraysCompareTest.java
+++ automated-tests-with-java/src/test/java/br/com/erudio/ArraysCompareTest.java
@@ -4,13 +4,15 @@ package br.com.erudio;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 class ArraysCompareTest {
 
     // test[System Under Test]_[Condition or State Change]_[Expected Result]
-    @DisplayName("Display name")
+    @DisplayName("Test sorted array")
     @Test
     void testSort_When_ArrayNumber_ShouldSortedNumberArray() {
         // Given - Arrange
@@ -22,6 +24,22 @@ class ArraysCompareTest {
 
         // Then - Assert
         assertArrayEquals(numbers, expected);
+    }
+
+    // test[System Under Test]_[Condition or State Change]_[Expected Result]
+    @DisplayName("Test array performance")
+    @Test
+    //@Timeout(1)
+    @Timeout(value = 15, unit = TimeUnit.MILLISECONDS)
+    void testSortPerformance() {
+        // Given - Arrange
+        int[] numbers = {25, 8, 21, 32, 3};
+        for (int i = 0; i < 100000; i++) {
+            // When - Act
+            numbers[0] = i;
+            // Then - Assert
+            Arrays.sort(numbers);
+        }
     }
 
 }
