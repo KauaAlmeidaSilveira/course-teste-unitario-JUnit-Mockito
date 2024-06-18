099ba9b0dd19b7d0439387a617dfd53c4f1a3830
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2024-06-18 08:14:58 -0300

    method parameterized with csvSource and csvFileSource

--- automated-tests-with-java/src/test/java/br/com/erudio/SimpleMathTestS4.java
+++ automated-tests-with-java/src/test/java/br/com/erudio/SimpleMathTestS4.java
@@ -5,10 +5,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
@@ -29,10 +26,16 @@ class SimpleMathTestS4 {
         System.out.println("Running @AfterEach");
     }
 
-    @DisplayName("Test 6.2 / 2 = 3.1")
+    @DisplayName("Test double division [firstNumber, secondNumber, expectedValue]")
     @ParameterizedTest
 //    @MethodSource("testDivisionInputParameters")
-    @MethodSource
+//    @MethodSource
+//    @CsvSource({
+//            "6.2, 2.0, 3.1",
+//            "71.0, 14.0, 5.07",
+//            "18.3, 3.1, 5.9"
+//    })
+    @CsvFileSource(resources = "/testDivision.csv")
     void testDivision(double firstNumber, double secondNumber, double expectedValue) {
         System.out.printf("%.2f/%.2f = %.2f !%n", firstNumber, secondNumber, expectedValue);
 
@@ -44,11 +47,11 @@ class SimpleMathTestS4 {
     }
 
     //    public static Stream<Arguments> testDivisionInputParameters() {
-    public static Stream<Arguments> testDivision() {
-        return Stream.of(
-                Arguments.of(6.2, 2.0, 3.1),
-                Arguments.of(71.0, 14.0, 5.07),
-                Arguments.of(18.3, 3.1, 5.9)
-        );
-    }
+//    public static Stream<Arguments> testDivision() {
+//        return Stream.of(
+//                Arguments.of(6.2, 2.0, 3.1),
+//                Arguments.of(71.0, 14.0, 5.07),
+//                Arguments.of(18.3, 3.1, 5.9)
+//        );
+//    }
 }
